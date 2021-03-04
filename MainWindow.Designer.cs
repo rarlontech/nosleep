5b17b44f027c9f1a6bc1c28303403f83c5361744
Ding Zhu <zd2100@hotmail.com>
2021-03-04 09:51:30 -0500

    initial commit

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace nosleep
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.toggleBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // toggleBtn
+            // 
+            this.toggleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toggleBtn.Location = new System.Drawing.Point(12, 6);
+            this.toggleBtn.Name = "toggleBtn";
+            this.toggleBtn.Size = new System.Drawing.Size(354, 131);
+            this.toggleBtn.TabIndex = 0;
+            this.toggleBtn.Text = "Toggle";
+            this.toggleBtn.UseVisualStyleBackColor = true;
+            this.toggleBtn.Click += new System.EventHandler(this.toggleBtn_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(378, 144);
+            this.Controls.Add(this.toggleBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 200);
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "MainWindow";
+            this.Text = "No Sleep";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button toggleBtn;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nosleep
+{
+    public partial class MainWindow : Form, IDisposable
+    {
+        private static bool NoSleepEnabled;
+
+        [Flags]
+        public enum ExecutionState : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern uint SetThreadExecutionState(ExecutionState esFlags);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            toggleVisual();
+        }
+
+        private void toggleBtn_Click(object sender, EventArgs e)
+        {
+            if (NoSleepEnabled)
+            {
+                this.DeactivateNoSleep();
+            }
+            else
+            {
+                this.ActivateNoSleep();
+            }
+
+            this.toggleVisual();
+        }
+
+        private void ActivateNoSleep()
+        {
+            SetThreadExecutionState(ExecutionState.ES_CONTINUOUS | ExecutionState.ES_DISPLAY_REQUIRED);
+            NoSleepEnabled = true;
+        }
+
+        private void DeactivateNoSleep()
+        {
+            SetThreadExecutionState(ExecutionState.ES_CONTINUOUS);
+            NoSleepEnabled = false;
+        }
+
+        private void toggleVisual()
+        {
+            if (NoSleepEnabled)
+            {
+                this.toggleBtn.BackColor = Color.Chartreuse;
+                this.toggleBtn.Text = "Activated";
+            }
+            else
+            {
+                this.toggleBtn.BackColor = Color.White;
+                this.toggleBtn.Text = "Deactivated";
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            this.DeactivateNoSleep();
+        }
+    }
+}
